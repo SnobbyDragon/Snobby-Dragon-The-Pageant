665939929e10f7f84f2386b89d37d77b6f7dd352
SnobbyDragon <angela.li.2019@gmail.com>
2018-07-23 21:10:00 -0400

    Add files via upload

--- Snobby Dragon_The Pageant/src/minigames/Pause.java
+++ Snobby Dragon_The Pageant/src/minigames/Pause.java
@@ -1,14 +1,21 @@
 package minigames;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 import general.SnobbyRunner;
 
 public class Pause extends JButton {
 	
+	private ImageIcon defaultIcon = new ImageIcon("Pause Button.gif"), rolloverIcon = new ImageIcon("Pause Button.gif"), pressedIcon = new ImageIcon("Pause Button.gif");
+	//TODO animate???
+	
 	public Pause() {
 		this.setVisible(false);
 		this.setBounds(SnobbyRunner.WIDTH - 60, 10, 50, 50);
 		this.setOpaque(true);
+		this.setIcon(defaultIcon);
+		this.setRolloverIcon(rolloverIcon);
+		this.setPressedIcon(pressedIcon);
 	}
 }
