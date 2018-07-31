b56eacf403a00652909b4d6c622a485e99cc45a1
SnobbyDragon <angela.li.2019@gmail.com>
2018-07-31 10:05:39 -0400

    Add files via upload

--- Snobby Dragon_The Pageant/src/minigames/Pause.java
+++ Snobby Dragon_The Pageant/src/minigames/Pause.java
@@ -1,15 +1,19 @@
 package minigames;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+import general.SnobbyPanel;
 import general.SnobbyRunner;
 
 public class Pause extends JButton {
-	
+
 	private ImageIcon defaultIcon = new ImageIcon("Pause Button.gif"), rolloverIcon = new ImageIcon("Pause Button.gif"), pressedIcon = new ImageIcon("Pause Button.gif");
 	//TODO animate???
-	
+
 	public Pause() {
 		this.setVisible(false);
 		this.setBounds(SnobbyRunner.WIDTH - 60, 10, 50, 50);
@@ -17,5 +21,18 @@ public class Pause extends JButton {
 		this.setIcon(defaultIcon);
 		this.setRolloverIcon(rolloverIcon);
 		this.setPressedIcon(pressedIcon);
+		this.addActionListener(new ActionListener() { //when button clicked, do something
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (SnobbyPanel.isRunning) {
+					SnobbyPanel.isRunning = false;
+				}
+				else {
+					SnobbyPanel.isRunning = true;
+					Pause.this.transferFocus(); //gives the game focus instead of the button so interactive functions work again
+				}
+				System.out.println(SnobbyPanel.isRunning);
+			}
+		});
 	}
 }
