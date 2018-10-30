d486d45cc423b9f0a395a02cf386bc2d15cf3b47
Ang <angela.li.2019@gmail.com>
2018-10-30 00:46:04 -0400

    animations still messed up. huge error when knocking down a peasant peasant

--- Snobby Dragon_The Pageant/src/minigames/glare/PeasantCutout.java
+++ Snobby Dragon_The Pageant/src/minigames/glare/PeasantCutout.java
@@ -14,7 +14,7 @@ public class PeasantCutout extends Thing {
 	private static final int[] RATES = new int[]{SnobbyPanel.FPS/5, SnobbyPanel.FPS/13, SnobbyPanel.FPS/18, SnobbyPanel.FPS/21};
 	private GifDecoder cutout = new GifDecoder(); //fall and up animation for when glared at
 	private int frame, rate; //counts the animation frames, rate at which the animation plays
-	private boolean isShook, isIdle; //peasant glared at??, not visible/needs to rise from the ashes of shookness or already up
+	private boolean isShook, isGettingUp, isDown; //peasant glared at??, rising from the ashes of shookness, is down
 	private Rectangle hitBox;
 	
 	public PeasantCutout(int x, int y) {
@@ -32,18 +32,24 @@ public class PeasantCutout extends Thing {
 				randomPeasant();
 			}
 		}
-		else if (!isIdle) { //is getting up
+		else if (isGettingUp) { //is getting up
 			frame--;
-			if (frame == 0) { //cutout is completely up
-				isIdle = true;
+			if (frame <= 0) { //cutout is completely up
+				isDown = false;
+				isGettingUp = false;
 			}
+		}
+		else if (isDown) {
+			shouldGetUp();
 		}
 		int n = frame/rate%cutout.getFrameCount(); //frame counter
 		window.drawImage(cutout.getFrame(n), getXPos(), getYPos(), null);
+		System.out.println(frame);
 	}
 
 	public void randomPeasant() { //sets this cutout to a random peasant type
-		isIdle = true; //doing nothing
+		isDown = true; //down
+		isGettingUp = false; //not getting up
 		isShook = false; //not shooketh till the Snobby glareth
 		frame = rate*cutout.getFrameCount(); //ready for get up reverse animation
 		
@@ -54,8 +60,9 @@ public class PeasantCutout extends Thing {
 	}
 	
 	public void shouldGetUp() { //determines if the peasant should get up now
-		if (Math.random() < 0.1) { //about 10% chance
-			isIdle = false;
+		if (Math.random() < 0.05) { //about 5% chance
+			isDown = false;
+			isGettingUp = true;
 		}
 	}
 	
